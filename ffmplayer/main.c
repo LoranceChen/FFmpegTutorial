182b7508b314caca5c9c5d99d761e974c5348f87
LoranceChen <wangchen@qiaobutang.com>
2016-10-25 23:55:06 +0800

    Initial Commit

---
+++ ffmplayer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ffmplayer
+//
+//  Created by LoranceChen on 16/10/25.
+//  Copyright © 2016年 LoranceChen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
